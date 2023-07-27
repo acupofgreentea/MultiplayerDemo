debe36dcb2d6391b19f40f8e7f64e3ded570ce7c
Akın Yıldırım <aknyldrm361@gmail.com>
2023-07-27 23:29:29 +0300

    camera controller

---
+++ Assets/Code/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using Cinemachine;
+using Fusion;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    
+    private CinemachineVirtualCamera cinemachine;
+
+    private void Awake()
+    {
+        cinemachine = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void Start()
+    {
+        PlayerSpawner.OnPlayerSpawned += OnPlayerSpawned;
+    }
+
+    private void OnPlayerSpawned(NetworkObject player)
+    {
+        cinemachine.Follow = player.transform;
+    }
+
+    private void OnDestroy()
+    {
+        PlayerSpawner.OnPlayerSpawned -= OnPlayerSpawned;
+    }
+}
